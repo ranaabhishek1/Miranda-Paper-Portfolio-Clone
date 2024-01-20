2af1c0d8b73558af6ffb7883e0b2dc4954df8f00
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-01-20 23:07:39 +0530

    completed

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ const scroll = new LocomotiveScroll({
 
 
 
-// var tl=gsap.timeline();
+var tl=gsap.timeline();
 
-// tl.to("#page1",{
-//     y:"100vh",
-//     scale:0.6,
-//     duration:0,
-// })
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0,
+})
 
 
 
-// tl.to("#page1",{
-//     y:"30vh",
-//     duration:1,
-//     delay:1,
+tl.to("#page1",{
+    y:"30vh",
+    duration:1,
+    delay:1,
 
-// })
+})
 
-// tl.to("#page1",{
-//     y:"0vh",
-//     rotate:360,
-//     scale:1,
-//     duration:0.7,
-// })
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:0.7,
+})
